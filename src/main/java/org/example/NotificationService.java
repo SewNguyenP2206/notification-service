a3dc11b42986cf76eaf7784c415194380b673c81
SewNguyenP2206 <NguyPhi974@gmail.com>
2023-12-20 11:20:52 +0700

    Fix1

--- src/main/java/org/example/NotificationService.java
+++ src/main/java/org/example/NotificationService.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 @Slf4j
+@EnableDiscoveryClient
+@EnableFeignClients
 public class NotificationService {
 
     @Autowired
